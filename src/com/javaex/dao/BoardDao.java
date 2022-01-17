1d4547f20b57256ed0105a3c8e6ff8bffd0d00f2
hwanginkyeong <inke0831@naver.com>
2022-01-17 10:36:52 +0900

    1st

--- src/com/javaex/dao/BoardDao.java
+++ src/com/javaex/dao/BoardDao.java
@@ -1,5 +1,57 @@
 package com.javaex.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BoardDao {
+	
+		// 0. import java.sql.*;
+		private Connection conn = null; 
+		private PreparedStatement pstmt = null; 
+		private ResultSet rs = null;
+		
+		private String driver = "oracle.jdbc.driver.OracleDriver";
+		private String url =  "jdbc:oracle:thin:@localhost:1521:xe";
+		private String id = "webdb"; 
+		private String pw = "webdb";
+		
+		//연결 
+		private void getConnection() {
+			
+			try {
+				// 1. JDBC 드라이버 (Oracle) 로딩
+				Class.forName(driver);
+
+				// 2. Connection 얻어오기
+				conn = DriverManager.getConnection(url, id, pw);
+				// System.out.println("접속성공");
+
+			} catch (ClassNotFoundException e) {
+				System.out.println("error: 드라이버 로딩 실패 - " + e);
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+		
+		//닫기
+		public void close() {
+			// 5. 자원정리
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
 
 }
