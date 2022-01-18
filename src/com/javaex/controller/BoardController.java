d931559ccba05cab778c20acc190e94acdac8eda
hwanginkyeong <inke0831@naver.com>
2022-01-18 09:38:56 +0900

    2nd

--- src/com/javaex/controller/BoardController.java
+++ src/com/javaex/controller/BoardController.java
@@ -1,13 +1,19 @@
 package com.javaex.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javaex.dao.BoardDao;
 import com.javaex.util.WebUtil;
+import com.javaex.vo.BoardVo;
 
 
 @WebServlet("/board")
@@ -18,18 +24,23 @@ public class BoardController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("/board");
 		
-		request.setCharacterEncoding("UTF-8");
+		//request.setCharacterEncoding("UTF-8");
+		
+		BoardDao boardDao = new BoardDao();
+		List<BoardVo> boardList = boardDao.getList();
 		
 		String action = request.getParameter("action");
+		System.out.println("test");
 		
 		
-		if("writeForm".equals(action)) {
-			System.out.println("/board>writeForm");
+		if("list".equals(action)) {
+			System.out.println("/board>list");
 			
-			WebUtil.forward(request, response, "/WEB-INF/views/borad/writeForm.jsp");
+			request.setAttribute("bList", boardList);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp");
+			rd.forward(request, response);
 			
-		}else if("write".equals(action)) {
-			System.out.println("/board>write");
+		
 		}
 		
 		
--- src/com/javaex/dao/BoardDao.java
+++ src/com/javaex/dao/BoardDao.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javaex.vo.BoardVo;
 
 public class BoardDao {
 	
@@ -53,5 +57,59 @@ public class BoardDao {
 				System.out.println("error:" + e);
 			}
 		}
+
+		public List<BoardVo> getList() {
+			
+				List<BoardVo> boardList = new ArrayList<BoardVo>();
+				
+				getConnection();
+				
+				try {
+					
+					// 3. SQL문 준비 / 바인딩 / 실행 
+					//문자열
+					String query ="";
+					query += " select   no, ";
+					query += "			title, ";
+					query += "			content, ";
+					query += "  		to_char(reg_date, 'yyyy-mm-dd') regDate, ";
+					query += "			user_no ";
+					query += " from board ";
+					
+					//쿼리
+					pstmt = conn.prepareStatement(query);
+					
+					//바인딩 물음표 없어서 X 
+					
+					//실행
+					rs = pstmt.executeQuery();     //리턴값이 달라서
+						
+					// 4.결과처리	
+					while(rs.next()) {
+						int no = rs.getInt("no");
+						String title = rs.getString("title");  //최종 컬럼명 별명이 있다면 별명 
+						String content = rs.getString("content");
+						int hit = rs.getInt("hit");
+						String regDate = rs.getString("regDate");
+						int userNo = rs.getInt("userNo");
+						
+						//vo로 묶어주기 최종적으로 리스트로 만들기 위해 하는 중 
+						BoardVo boardVo = new BoardVo(no, title, content,  hit, regDate, userNo);
+						boardList.add(boardVo);
+					} 
+					
+			}catch (SQLException e) { 
+						System.out.println("error:" + e);
+			} 
+			
+				
+			return boardList;
+			
+		}
+		
+		
+		
+		
+		
 
 }
